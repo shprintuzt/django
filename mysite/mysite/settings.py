fb25d9786201a491981c5de68b1bf5847639c29b
LAPTOP-PSIVF9T5\T-GOTOH <t-gotoh@ist.osaka-u.ac.jp>
2018-05-14 20:55:04 +0900

    Revert "ユーザモデルを変更"
    This reverts commit adbc44dd0903281bde35a97f693d4e573a14772f.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -41,8 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
-AUTH_USER_MODEL = 'accounts.User'
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
